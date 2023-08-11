3fadf477c021fda107cc028e522122a5efe86ca6
root <root@nuru-PC.localdomain>
2023-08-11 02:18:01 +0100

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * use the put fxn to print a line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts(\""Programming is like building a multilingual puzzle\n");
+return (0);
+}
+
+
