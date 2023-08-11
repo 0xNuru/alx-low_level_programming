cb392417babf01167cdfcb9d905895feebee69be
root <root@nuru-PC.localdomain>
2023-08-11 02:43:02 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * use the printf fxn to print a line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+			return (0);
+}
+
