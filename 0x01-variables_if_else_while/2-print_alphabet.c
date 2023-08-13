e7ac90b40069ef289f8d6cec7b841acf843cb946
root <nurudeenmyself@yahoo.com>
2023-08-14 00:28:22 +0100

    add  2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: a program that prints the alphabet
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
