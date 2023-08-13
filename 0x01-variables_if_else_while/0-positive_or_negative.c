ee2daa1f5e334dd5aaffe2c38bae912d11149987
root <nurudeenmyself@yahoo.com>
2023-08-13 23:05:23 +0100

    add  0-positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers go there */
+#include <stdio.h>
+/**
+ *main - prints the sign
+ * Description: This program will assign a random number to
+ * a variable n each
+ * time it is executed
+ * it will also print the sign attached to the number each
+ * time the code is run
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes here */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
