944da2d580e7287d86a02a06f89b5f246aef1700
root <nurudeenmyself@yahoo.com>
2023-08-14 12:04:45 +0100

    add  4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all the single digit numbers of base 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
