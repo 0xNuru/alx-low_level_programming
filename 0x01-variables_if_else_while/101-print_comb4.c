974547e4597290ed51db25c7ba83087c7804dbfd
root <nurudeenmyself@yahoo.com>
2023-08-17 14:11:01 +0100

    add 0-main.c, main.h

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,17 +4,17 @@
  *
  * Description: a program that prints all possible
  * different combinations of three digits.
- * Result: 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		for (j = i + 1; j < 8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1; k < 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
