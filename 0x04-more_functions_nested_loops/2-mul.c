9d607828e135d8b7e465af5f7bbad13fdc378741
root <nurudeenmyself@yahoo.com>
2023-08-18 17:36:06 +0100

    add 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+* mul - functin name
+* @a: first number to be multiplied
+* @b: second number to be multiplied
+* Description: a function that multiplies two integers.
+* Return: result of multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
