485a1fc1bcbe65e339b960a4040f4a89d70f0cb1
root <nurudeenmyself@yahoo.com>
2023-08-18 17:20:32 +0100

    add 1-isdigit.c1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+* main - entry point
+* @c: int to be checked
+*
+* Description: a function that prints the alphabet
+* Return: 0 (success)
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c >= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
