ada2b7f72b8c9b8b9c358ef02680cd1b32598897
root <nurudeenmyself@yahoo.com>
2023-08-18 19:11:54 +0100

    add 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+/**
+* more_numbers - function name
+*
+* Description: a function that prints the numbers 0 to 14 x10
+* Return: void
+*/
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('0' + (j / 10));
+			}
+			_putchar('0' + (j % 10);
+		}
+		_putchar('\n');
+	}
+}
