e90e504657a04642bab921a04c677ac5fe0c8ad8
root <nurudeenmyself@yahoo.com>
2023-08-22 19:53:50 +0100

    add 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - function name
+ * @n: integer to be reset
+ *
+ * Description: a function that resets the value of an int
+ * to 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
