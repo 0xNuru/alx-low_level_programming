bcb9ad394b05b8cefd45d0efc850f137cfc75703
root <nurudeenmyself@yahoo.com>
2023-08-22 21:59:50 +0100

    add 4-rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
 * print_rev - function name
-* @s = str
+* @s: str
 *
 * Description: a function that prints string in reverse
 * Return: void
@@ -22,7 +22,7 @@ void print_rev(char *s)
 	s = s - i;
 	length = i--;
 
-	for (j = length; j >= 0; j--)
+	for (j = length; j > 0; j--)
 	{
 		_putchar(*(j + s));
 	}
