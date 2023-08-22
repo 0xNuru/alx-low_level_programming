41cf41950b259ada815e987977f21778ba35ab2e
root <nurudeenmyself@yahoo.com>
2023-08-22 23:26:05 +0100

    add 6-puts2 betty mod

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,7 @@
 
 /**
 * puts2 - function name
+* @str: string
 *
 * Description: a function that prints every other char
 * Return: Void
@@ -12,7 +13,7 @@ void puts2(char *str)
 	int i = 0;
 
 	while (*str)
-	
+
 	{
 		if (i % 2 == 0)
 		{
