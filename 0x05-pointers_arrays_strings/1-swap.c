b1680381b8999a54814a6fafb00149d5a320b2ec
root <nurudeenmyself@yahoo.com>
2023-08-22 20:04:01 +0100

    1-main.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+* swap_int - function name
+* @a: first int to be swapped
+* @b: second function to be swapped
+*
+* Description: a function that swaps the values of a and b
+* Return: 0 (success)
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	a = *b
+	b = temp;
+}
