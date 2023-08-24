4df24874d4fc0b894b255a99357732ff68d04eb6
root <nurudeenmyself@yahoo.com>
2023-08-24 19:01:23 +0100

    add 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,11 +13,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int result;
+	
 	while ((*s1 && *s2) && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	result  = (*s1 - s2);
+	result  = (*s1 - *s2);
 	return (result);
 }
