4dda99931126a67cbc507ae56ef479bd8413de16
root <nurudeenmyself@yahoo.com>
2023-08-24 22:43:37 +0100

    add 7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,20 +6,20 @@
 * Return: address of result
 */
 
-char *leet(char *x)
+char *leet(char *str)
 {
 	int i, j;
 	char x[] = "aAeEoOTlL", y[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (f = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (str[i] == x[j])
 			{
-				str[i] = y[j]
+				str[i] = y[j];
 			}
 		}
 	}
-	return (str);
+	 return (str);
 }
