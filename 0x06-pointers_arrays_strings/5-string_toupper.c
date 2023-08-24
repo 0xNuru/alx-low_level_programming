23ac3f7b2ac6643732ef59bb8b93eeee592fb985
root <nurudeenmyself@yahoo.com>
2023-08-24 21:48:55 +0100

    add 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+* string_toupper - function name
+*
+* Description: a function that converts lowercase to upper
+* Return: address to resulting str
+*/
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
