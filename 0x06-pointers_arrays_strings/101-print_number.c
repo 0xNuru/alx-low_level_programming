8abca85ca54109e587d9362636764398b779b4f7
root <nurudeenmyself@yahoo.com>
2023-08-26 22:58:05 +0100

    add 101-print_num long mod

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,16 +9,19 @@
 
 void print_number(int n)
 {
+	unsigned int long_int;
+
+	long_int = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		long_int = -n;
 	}
 
-	if (n / 10 != 0)
+	if (long_int / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(long_int / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((long_int % 10) + '0');
 
 }
