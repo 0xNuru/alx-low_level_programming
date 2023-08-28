2ea41865ce5095f446fe6349e4754d20a3003034
root <nurudeenmyself@yahoo.com>
2023-08-28 22:46:06 +0100

    add 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+/**
+* set_string - function name
+* @s: double  pointer
+* @to: dest pointer
+*
+* Description: a function that sets the value of a pointer
+* to a char
+* Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
