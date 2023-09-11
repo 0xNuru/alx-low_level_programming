6043f8082d8af8effeef7ee2f6add35562ce927e
0xNuru <nurudeenmyself@yahoo.com>
2023-09-11 19:01:22 +0100

    add 5-free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+
+/**
+* free_dog - function name
+* @d: pointer to struct dog object
+*
+* Description: a function that frees dogs
+* Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+	}
+}
