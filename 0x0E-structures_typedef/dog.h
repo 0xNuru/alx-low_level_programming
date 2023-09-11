ec86adeac1fb50b4023fa0e5ecef1ded2c3899c7
0xNuru <nurudeenmyself@yahoo.com>
2023-09-11 17:09:03 +0100

    add 0-dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+* struct dog - struct prototype
+* @name: dog's name
+* @owner: owner's name
+* @age: dog's age
+*
+* Description: a dog data type
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
