83dcd627b74a86d8707e5cab060d9c643796a8a7
0xNuru <nurudeenmyself@yahoo.com>
2023-09-13 19:06:52 +0100

    3-calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,15 +14,19 @@
 
 int main(int argc, char  *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-	char *op = argv[2];
-
+	int num1, num2;
+	char *op;
+	printf("%d\n", argc);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
