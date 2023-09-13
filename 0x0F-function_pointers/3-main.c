3564d0712c96a11573dccf814211ea5621cee2b4
0xNuru <nurudeenmyself@yahoo.com>
2023-09-13 18:36:22 +0100

    3-calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+* main - entry point
+* @argc: argument count
+* @argv: array of args
+*
+* Description: a program that performs simple operations
+* usage: calc num1 operator num2
+* Return: 0 (success)
+*/
+
+int main(int argc, char  *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+	char *op = argv[2];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+/**
+* op_add - function name
+* @a: first operand
+* @b: second operand
+*
+* Description: a function that adds two ints
+* Return: sum of two ints
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - function name
+ * @a: first operand
+ * @b: second operand
+ *
+ * Description: a function that subtracts two ints
+ * Return: difference between two ints
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - function name
+ * @a: first operand
+ * @b: second operand
+ *
+ * Description: a function that multiplies two ints
+ * Return: product of two ints
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - function name
+ * @a: first operand
+ * @b: second operand
+ *
+ * Description: a function that div two ints
+ * Return: quotient of two ints
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - function name
+ * @a: first operand
+ * @b: second operand
+ *
+ * Description: a function that finds the mod of two ints
+ * Return: remainder of two ints
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
