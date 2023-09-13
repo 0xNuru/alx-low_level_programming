3b27e9e91814a017f8c5b1ecf103feb7b1c47f10
0xNuru <nurudeenmyself@yahoo.com>
2023-09-13 18:46:03 +0100

    3-calc

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
 #ifndef _3_calc_h
-#define _3_calc_h 
+#define _3_calc_h
 
 /**
  * struct op - Struct op
@@ -9,8 +9,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
