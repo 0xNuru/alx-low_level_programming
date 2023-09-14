9fcda7a1ffe93c86617a25dce2d98ae67346b8c8
0xNuru <nurudeenmyself@yahoo.com>
2023-09-14 21:21:40 +0100

    3-print_all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,22 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+* struct formatter
+* @identifier: an identifier to detect data type
+* @print: a pointer to a function that prints the detected
+* data type
+*/
+
+typedef struct formatter
+{
+	char *identifier;
+	void (*print)(va_list arg);
+} fmt;
 #endif
