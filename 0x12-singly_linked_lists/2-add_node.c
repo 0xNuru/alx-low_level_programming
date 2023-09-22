78b6357a105697555bf71dd8529852d1361e2569
0xNuru <0xNuru@gmail.com>
2023-09-22 16:49:04 +0100

    add 2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,8 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *newnode;
 
 	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		exit(98);
 	newnode->next = *head;
 	newnode->len = strlen(str);
 	newnode->str = strdup(str);
