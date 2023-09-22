2bc3c9ea2160c523c17bc14c4d3b75fee4f3dacd
0xNuru <0xNuru@gmail.com>
2023-09-22 16:46:05 +0100

    add 2-add_node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef lists_h
 #define lists_h
+#include <string.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,4 +21,5 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
