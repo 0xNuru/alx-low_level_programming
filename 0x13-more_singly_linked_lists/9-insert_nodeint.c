33821fafb35716e9d2c5c5fba345a158896df36d
0xNuru <0xNuru@gmail.com>
2023-09-26 17:02:27 +0100

    add 9-ins@idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
 * insert_nodeint_at_index - a function that inserts a new node
 * at a given index
@@ -12,18 +11,18 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *temp, *new_node;
 	unsigned int i;
-	listint_t *temp;
-	listint_t *new_node;
 
 	if (*head == NULL)
 		return (NULL);
 
 	temp = (*head);
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->next = NULL;
 	new_node->n = n;
 
@@ -33,11 +32,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = new_node;
 		return (new_node);
 	}
-
 	for (i = 0; i < idx; i++)
 	{
 		temp = temp->next;
-
 		if (temp == NULL)
 		{
 			free(new_node);
@@ -46,11 +43,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->next = temp;
 	temp = *head;
-
+	
+	if (idx == 1)
+		temp->next = new_node;
 	for (i = 0; i < (idx - 1); i++)
 		temp = temp->next;
 
 	temp->next = new_node;
-
 	return (new_node);
 }
