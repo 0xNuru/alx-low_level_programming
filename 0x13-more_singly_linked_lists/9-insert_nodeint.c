b3b5ad7938a906ecd91bf565628b57130cb02bf8
0xNuru <0xNuru@gmail.com>
2023-09-26 17:13:40 +0100

    add 9-ins@idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
@@ -28,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = new_node;
 		return (new_node);
 	}
-	
+
 	temp = (*head);
 	for (; idx != 1; --idx)
 	{
