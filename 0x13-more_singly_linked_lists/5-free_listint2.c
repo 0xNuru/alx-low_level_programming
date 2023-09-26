67898ac0ce1d598b4c74bcf1a97740fd5e328991
0xNuru <0xNuru@gmail.com>
2023-09-26 12:11:28 +0100

    add 5-free_ll

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - a function that frees a linked list
+* @head: address of a pointer to the head of the LL
+*
+* Return: 0 (success)
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
