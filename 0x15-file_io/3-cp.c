6f241dab62c587505cd8879cb0c9ff70db489f55
0xNuru <0xNuru@gmail.com>
2023-10-02 22:32:24 +0100

    add 3-cp betty mod

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,37 @@
 #include "main.h"
 
+/**
+* error_and_exit - an error handling function
+* @exit_code: exit code
+* @message: error message
+*
+*/
 void error_and_exit(int exit_code, const char *message)
 {
 	dprintf(STDERR_FILENO, "%s\n", message);
 	exit(exit_code);
 }
+
+/**
+* _error_and_exit - an error handling function
+* @file_from: file to copy from
+* @file_to: file to copy to
+* @argv: argument vector
+*/
+void _error_and_exit(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
 
 
 /**
@@ -23,17 +50,9 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		error_and_exit(97, "Usage: cp file_from file_to");
 	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	_error_and_exit(fd1, fd2, argv);
+
 	while (bytes_read == BYTES)
 	{
 		bytes_read = read(fd1, buffer, BYTES);
