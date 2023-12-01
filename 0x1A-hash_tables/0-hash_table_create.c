59c4e40df1509fc81adbab18e4558b29d4698898
0xNuru <nurudeenmyself@yahoo.com>
2023-12-01 11:39:52 +0100

    add readme and 0-hsh.c mod2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
@@ -19,6 +20,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
+	if (table->array == NULL) 
+	{
+    		free(table);
+    		return (NULL);
+	}
+
+	for (i = 0; i < size; i++) 
+	    table->array[i] = NULL;
 
 	return (table);
 }
